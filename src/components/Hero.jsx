178bfe4effbf68e8fe540b3ffcbfb66ea52e29a4
alex-brown-1 <alexbrown26@vt.edu>
2023-11-13 05:40:17 -0500

    Got rid of button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,5 @@
 import styles from '../style'
 import { discount, robot } from '../assets'
-import GetStarted from './GetStarted'
 
 const Hero = () => {
   return (
@@ -22,9 +21,6 @@ const Hero = () => {
             The Next <br className='sm:block hidden'/> {" "}
             <span className='text-gradient'>Generation</span> {" "}
           </h1>
-          <div className='ss:flex hidden md:mr-4 mr-0'>
-            <GetStarted/>
-          </div>
         </div>
         <h1 className='font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full'>
           Of Fitness
@@ -43,9 +39,6 @@ const Hero = () => {
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
         <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
         <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient"/>
-      </div>
-      <div className={`ss:hidden ${styles.flexCenter}`}>
-        <GetStarted/>
       </div>
     </section>
   )
